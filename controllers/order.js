a88130c7c0e9c3dc327b2b813df44301c72683aa
Knox <phuongle6428@gmail.com>
2023-01-02 18:20:39 +0700

    Update

--- controllers/order.js
+++ controllers/order.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.set('view engine', 'ejs')
-app.set('views', require.main.path + '/utility')
+app.set('views', '../utility')
 
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport')
